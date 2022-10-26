9883ae223383378982f0b39f9e647dbc56011950
jacekk024 <jacekkotra98@gmail.com>
2022-10-27 01:09:04 +0200

    add Variations and Combinations alg, correct nQueen
    correct nQueen - gives one correcr variant

--- Algorithms/Exercise2/Queen.cs
+++ Algorithms/Exercise2/Queen.cs
@@ -11,23 +11,32 @@ namespace Algorithms.Exercise2
     {
         private Stopwatch watch = new Stopwatch();
         private int[,] chessBoard;
-        private int boardSize;
         private int N;
 
-        private int[] x = new int[8]; // pozycja w i-tej kolumnie
-        private bool[] a = new bool[8]; //  brak hetmana w j−tym wierszu  
-        private bool[] b = new bool[15]; // brak hetmana na k−tej przekątnej
-        private bool[] c = new bool[15]; //  brak hetmana na k−tej przekątnej 
+        private int[] x; // pozycja w i-tej kolumnie
+        private bool[] a; //  brak hetmana w j−tym wierszu  
+        private bool[] b; // brak hetmana na k−tej przekątnej
+        private bool[] c; //  brak hetmana na k−tej przekątnej 
+
+        private int n;
 
 
         public Queen(int N) 
         {
             this.N = N;
-            boardSize = N * N;
-            
-            for(int i = 0; i < N; i++)
+            chessBoard = new int[N, N];
+
+            n = N - 1;
+
+            x= new int[N];
+            a= new bool[N]; 
+            b= new bool[2*N-1];
+            c= new bool[2 *N-1];
+
+
+            for (int i = 0; i < N; i++)
                 a[i] = true;
-            for (int j = 0; j < 15; j++) {
+            for (int j = 0; j < 2*N-1; j++) {
                 b[j] = true;
                 c[j] = true;
             }
@@ -37,52 +46,61 @@ namespace Algorithms.Exercise2
         void PrintQueenPosition() 
         {
 
-            for (int i = 0; i < N; i++)
+            for (int i = 0, j = 1; i < N; j++, i++)
             {
-                Console.WriteLine(x[i]);
+                chessBoard[i, x[i]] = j;
             }
 
-            //for (int i = 0; i < N; i++)
-            //{
-            //    for (int j = 0; j < N; j++)
-            //        Console.Write(chessBoard[i,j]);
-            //    Console.WriteLine();
-            //}
-        
+            for (int i = 0; i < N; i++)
+                Console.Write(x[i]+1 + " ");
+            Console.WriteLine();
+
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                    Console.Write(chessBoard[i, j] + "");
+                Console.WriteLine();
+            }
+
         }
 
         public void StartSetQueen()
         {
             bool q = false;
-            Try8Queen(1, q);
+            TryNQueen(0,ref q);
             PrintQueenPosition();
         }
 
 
-        public void Try8Queen(int i,bool q) 
+        public void TryNQueen(int i,ref bool q) 
         {
-            int j;
+            int j = -1;
 
-            j = -1;
+          //  j = -1;
             do
             {
                 j = j + 1; 
                 q = false;
-                 if (a[j] && b[i+j] && c[i-j + 7])
+                 if (a[j] && b[i+j] && c[i-j + n])
                  {
                      x[i] = j;
-                     a[j] = false; b[i + j] = false; c[i-j + 7] = false;
-                     if (i < 7)
+
+                     a[j] = false;
+                     b[i + j] = false;
+                     c[i-j + n] = false;
+                     if (i < n)
                      {
-                        Try8Queen(i + 1, q);
+                        TryNQueen(i + 1,ref q);
                          if (!q) {
                             a[j] = true;
-                            b[i + j] = true;
-                            c[i-j + 7] = true;
-                            }
-                     } else q = true;
+                            b[i+j] = true;
+                            c[i-j+n] = true;
+                         }
+                     } 
+                     else q = true;
                  }
-            } while (!q && (j != 7));
+            } while (!q && (j != n));
         }
     }
 }
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -16,19 +16,37 @@ namespace Algorithms
                 float[] data2 = { 3.1F, 1.4F, 4.1F }; 
                 string[] data3 = { "raz", "dwa", "trzy" };
 
-                int[] timeUsed = {1,2};
+                int[] timeUsed = {1,1};
                 Exercise1.Permutation<int> perm = new Permutation<int>();
                 perm.SetPermutationValues(data, timeUsed);
                 perm.ShowPermutationResult();
             }
+
+            //Combination
+            if (false) 
+            {
+                Exercise1.Combination<int> comb = new Exercise1.Combination<int>();
+                int[] data = { 1, 2, 3, 4, 5 };
+                comb.SetCombinationValues(data,3);         
+            }
+
+            //Variation
+            if (true)
+            {
+                Exercise1.Variation<int> variation = new Exercise1.Variation<int>();
+                int[] data = { 1, 2, 3, 4};
+                int[] timeUsed = { 1, 1,1,1 };
+                variation.SetVariationValues(data,3, timeUsed);
+            }
+
             //Knight
             if (false) 
             {
                 Exercise2.Knight knight = new Exercise2.Knight(5);
                 knight.StartKnightTour();
             }
-
-            if (true)
+            //NQueen
+            if (false)
             {
                 Exercise2.Queen queen = new Exercise2.Queen(4);
                 queen.StartSetQueen();
